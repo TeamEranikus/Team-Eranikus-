4b6f15a4d33c2927fae45232b49261826ccd5511
elitasheva <elica.tasheva@gmail.com>
2016-06-07 17:35:53 +0300

    added inventory images

--- EscapeCode/src/sample/scenes/demoLevel/DemoController.java
+++ EscapeCode/src/sample/scenes/demoLevel/DemoController.java
@@ -1,5 +1,15 @@
 package sample.scenes.demoLevel;
 
+import javafx.event.Event;
+
 public class DemoController {
 
+    public void onMouseClickKeyItem(Event event) {
+    }
+
+    public void onMouseClickPapierItem(Event event) {
+    }
+
+    public void onMouseClickBookItem(Event event) {
+    }
 }
