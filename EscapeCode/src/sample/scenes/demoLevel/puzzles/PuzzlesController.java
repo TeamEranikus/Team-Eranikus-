f3990e5c5b2fe7fd01b9e4e113d50390bd44a680
NerdLady <dimana_t@abv.bg>
2016-06-10 12:18:56 +0300

    Added method centerImage in PuzzleConroller

--- EscapeCode/src/sample/scenes/demoLevel/puzzles/PuzzlesController.java
+++ EscapeCode/src/sample/scenes/demoLevel/puzzles/PuzzlesController.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
@@ -25,11 +26,44 @@ public class PuzzlesController implements Initializable {
     @FXML
     TextField userAnswer;
 
-    public void giveHint(ActionEvent actionEvent) {
-    }
+    private String correctAnswer;
+    private Image image;
+    private String descriptionText;
+    private String puzzleHint;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        centerImage();
+    }
 
+    public void giveHint(ActionEvent actionEvent) {
+    }
+
+
+    public void checkAnswer(ActionEvent actionEvent) {
+    }
+
+    private void centerImage() {
+        if (image != null) {
+            double width = 0;
+            double height = 0;
+
+            double ratioX = puzzleImage.getFitWidth() / image.getWidth();
+            double ratioY = puzzleImage.getFitHeight() / image.getHeight();
+
+            double reduceCoeff = 0;
+            if (ratioX >= ratioY) {
+                reduceCoeff = ratioY;
+            } else {
+                reduceCoeff = ratioX;
+            }
+
+            width = image.getWidth() * reduceCoeff;
+            height = image.getHeight() * reduceCoeff;
+
+            puzzleImage.setX((puzzleImage.getFitWidth() - width) / 2);
+            puzzleImage.setY((puzzleImage.getFitHeight() - height) / 2);
+
+        }
     }
 }
