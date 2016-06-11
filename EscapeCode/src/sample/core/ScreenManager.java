6e2028f23400df4acafb64e633da320ffe7583e9
Ivan Ivanov <ivanov.ivan_@abv.bg>
2016-06-11 21:01:21 +0300

    Make windows resizible

--- EscapeCode/src/sample/core/ScreenManager.java
+++ EscapeCode/src/sample/core/ScreenManager.java
@@ -1,8 +1,11 @@
 package sample.core;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import sample.utils.Constants;
 
@@ -15,8 +18,33 @@ public class ScreenManager {
     public void setPrimaryStage(Stage primaryStage, String FXMLPath) throws IOException {
         primaryStage.setTitle(Constants.TITLE);
         root = FXMLLoader.load(getClass().getResource("../scenes/" + FXMLPath));
-        primaryStage.setResizable(false);
-        primaryStage.setScene(new Scene(root));
+        Region contentRootRegion = (Region) FXMLLoader.load(getClass().getResource("../scenes/" + FXMLPath));
+        double origW = 1920.0;
+        double origH = 1080.0;
+
+        if (contentRootRegion.getPrefWidth() == contentRootRegion.USE_COMPUTED_SIZE){
+            contentRootRegion.setPrefWidth(origW);
+        } else {
+            origW = contentRootRegion.getPrefWidth();
+        }
+
+        if (contentRootRegion.getPrefHeight() == contentRootRegion.USE_COMPUTED_SIZE){
+            contentRootRegion.setPrefHeight(origH);
+        } else {
+            origH = contentRootRegion.getPrefHeight();
+        }
+        Group group = new Group(contentRootRegion);
+
+        //Place the Group in a StackPane, which will keep it centered
+        StackPane rootPane = new StackPane();
+        rootPane.getChildren().add( group );
+        Scene scene = new Scene( rootPane, origW, origH );
+        //Bind the scene's width and height to the scaling parameters on the group
+        group.scaleXProperty().bind( scene.widthProperty().divide( origW ) );
+        group.scaleYProperty().bind( scene.heightProperty().divide( origH ) );
+        //Set the scene to the window (stage) and show it
+       // primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.centerOnScreen();
     }
@@ -24,8 +52,11 @@ public class ScreenManager {
     public <T> T loadSceneToPrimaryStage(Stage currentStage, String FXMLPath) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../scenes/" + FXMLPath));
         try {
+            Region region = FXMLLoader.load(getClass().getResource("../scenes/" + FXMLPath));
             root = fxmlLoader.load();
-            currentStage.setScene(new Scene(root));
+            //root.getChildren().add(region);
+            Scene scene = new Scene(root);
+            currentStage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
         }
